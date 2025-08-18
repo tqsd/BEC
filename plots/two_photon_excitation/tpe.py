4fd24b9e9bed4597b95d80a99fc8b568061ac69b
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2025-08-18 02:31:49 +0200

    added plotter class

--- plots/two_photon_excitation/tpe.py
+++ plots/two_photon_excitation/tpe.py
@@ -1,7 +1,17 @@
+from qutip import Options
+import matplotlib.pyplot as plt
+import numpy as np
+from qutip import Options, mesolve, Qobj
 from photon_weave.state.envelope import Envelope
+from photon_weave.state.composite_envelope import CompositeEnvelope
+from photon_weave.state.polarization import PolarizationLabel
+
+from bec.light.classical import ClassicalTwoPhotonDrive
+from bec.light.envelopes import GaussianEnvelope
 from bec.operators.qd_operators import QDState
 from bec.quantum_dot.params import CavityParams, DipoleParams, EnergyLevels
 from bec.quantum_dot.qd_photon_weave import QuantumDotSystem
+from bec.plots.plotter import QDPlotter
 
 # --- Step 1: Make energy levels & QD system ---
 
@@ -12,7 +22,10 @@ QD = QuantumDotSystem(
     EL, initial_state=QDState.G, cavity_params=CP, dipole_params=DP
 )
 
-FLY_ENV_1 = Envelope()
-FLY_ENV_1.wavelength = 918.4
 
-QD.register_flying_mode(FLY_ENV_1, label="first")
+DRIVE = ClassicalTwoPhotonDrive(
+    omega=GaussianEnvelope(t0=3, sigma=0.5, area=np.pi / 2), detuning=0
+)
+
+plotter = QDPlotter(QD, classical_2g=DRIVE, tlist=np.linspace(0, 10, 5000))
+res = plotter.run_and_plot(filename="classical_two_photon_drive", show_top=True)
